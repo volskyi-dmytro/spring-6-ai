c5459983298eb9a51f7fa695b924e12455e3e6e4
@stpunk47 <stpunk47@gmail.com>
2025-03-17 20:19:59 +0200

    Initial commit

---
+++ src/main/java/com/stpunk47/spring_6_ai/Spring6AiApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6AiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6AiApplication.class, args);
+	}
+
+}
